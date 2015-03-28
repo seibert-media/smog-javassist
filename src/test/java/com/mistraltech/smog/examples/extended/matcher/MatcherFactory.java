9fe60c97d3edbe2488a3341bc84d8c561dc9a9be
Richard Wilson <richardjwilson@users.noreply.github.com>
2015-03-28 14:59:19 +0000

    Add further examples/tests for extensible matcher interfaces.

---
+++ src/test/java/com/mistraltech/smog/examples/extended/matcher/MatcherFactory.java
@@ -0,0 +1,18 @@
+package com.mistraltech.smog.examples.extended.matcher;
+
+import com.mistraltech.smog.examples.model.Addressee;
+import com.mistraltech.smog.examples.model.Person;
+
+import static com.mistraltech.smog.proxy.javassist.JavassistMatcherGeneratorImpl.matcherOf;
+
+public class MatcherFactory {
+    @SuppressWarnings("unchecked")
+    public static AddresseeMatcher<?, Addressee> anAddresseeThat() {
+        return matcherOf(AddresseeMatcher.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static PersonMatcher<?, Person> aPersonThat() {
+        return matcherOf(PersonMatcher.class);
+    }
+}
---
+++ src/test/java/com/mistraltech/smog/examples/extended/matcher/PersonMatcher.java
@@ -0,0 +1,17 @@
+package com.mistraltech.smog.examples.extended.matcher;
+
+import com.mistraltech.smog.core.annotation.Matches;
+import com.mistraltech.smog.examples.model.Person;
+import com.mistraltech.smog.examples.model.Phone;
+import org.hamcrest.Matcher;
+
+import java.util.List;
+
+@Matches(value = Person.class, description = "a Person")
+public interface PersonMatcher<R extends PersonMatcher<R, T>, T extends Person> extends AddresseeMatcher<R, T> {
+    R hasAge(int age);
+
+    R hasAge(Matcher<? super Integer> ageMatcher);
+
+    R hasPhoneList(Matcher<? super List<? extends Phone>> phoneListMatcher);
+}
